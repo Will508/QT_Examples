ae832cbfe51ff26c0ed31dabfec062823ddba410
will <will@gmail.com>
2023-08-27 19:38:01 +0800

    signal and slot finished

--- 03_signal_slot/mainwindow.cpp
+++ 03_signal_slot/mainwindow.cpp
@@ -3,9 +3,27 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    this->resize(800, 480);             // 设置窗口大小为800*480
+    pushButton = new QPushButton(this); // 实例化pushbutton对象
+    pushButton->setText("button 1");
+
+    //链接信号和槽，点击信号链接点击槽输出文本信号改变，文本信号改变信号链接按钮文本改变
+    connect(pushButton,SIGNAL(clicked()),this,SLOT(pushButtonClicked()));
+    connect(this,SIGNAL(pushButtonTextChanged()),this,SLOT(changeButtonText()));
+
 }
 
 MainWindow::~MainWindow()
 {
 }
 
+void MainWindow::pushButtonClicked()//按钮点击
+{
+    //使用emit发射信号
+    emit pushButtonTextChanged();
+}
+
+void MainWindow::changeButtonText()//按钮文本改变
+{
+    pushButton->setText("clicked!");
+}
--- 03_signal_slot/mainwindow.h
+++ 03_signal_slot/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton> // 声明一个QPushButton宏
 
 class MainWindow : public QMainWindow
 {
@@ -10,5 +11,16 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+signals:
+    void pushButtonTextChanged(); // 声明一个信号，无需定义
+
+public slots:
+    void changeButtonText(); // 声明一个槽函数
+
+    void pushButtonClicked(); // 声明按钮点击的槽函数
+
+private:
+    QPushButton *pushButton; // 声明一个pushButton对象
 };
 #endif // MAINWINDOW_H
